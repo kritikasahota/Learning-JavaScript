3a24dc90257c10573e8420eadd725fdfdf015db8
Kritikasahota <sahotakritika029@gmail.com>
2026-07-08 11:56:51 +0530

    demonstrated else-if statement for decision making

---
+++ 02_Conditional Statements/03_elseIfStatment.js
@@ -0,0 +1,36 @@
+// ======================================
+// JavaScript else if Statement (Notes)
+// ======================================
+
+// The else if statement is used to check multiple conditions one after another.
+//It is executed only if the previous condition is false.
+
+// Syntax:
+// if (condition1) {
+//     // Executes if condition1 is true
+// } else if (condition2) {
+//     // Executes if condition2 is true
+// } else {
+//     // Executes if all conditions are false
+// }
+
+
+// ======================================
+// Example: Age Category
+// ======================================
+
+let age = 25;
+
+if (age <= 18) {
+    console.log("Junior");
+}
+else if (age >= 60) {
+    console.log("Senior");
+}
+else {
+    console.log("Middle");
+}
+
+
+
+
