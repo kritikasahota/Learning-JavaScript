dc3f37bccc13293773239d2c0cc64fe0bd22e633
Kritikasahota <sahotakritika029@gmail.com>
2026-07-08 11:58:59 +0530

    demostrated ternary operator as a shorthand for if-else

---
+++ 02_Conditional Statements/05_ternaryOperator.js
@@ -0,0 +1,27 @@
+// ======================================
+// JavaScript Ternary Operator (Notes)
+// ======================================
+
+// The ternary operator is a shorthand for the if...else statement.
+// It checks a condition.
+// If the condition is true, the first value is returned.
+// If the condition is false, the second value is returned.
+
+
+// ======================================
+// Syntax
+// ======================================
+
+// condition ? expression1 : expression2;
+
+// ======================================
+// Example: Pass or Fail
+// ======================================
+
+let marks = 45;
+let result = marks < 45 ? "Fail" : "Pass";
+console.log(result);
+
+// If marks are less than 45 → "Fail"
+// Otherwise → "Pass"
+
