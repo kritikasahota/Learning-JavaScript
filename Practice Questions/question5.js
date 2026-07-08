5820891acbd59df6deada5a5cd14a678a2d1e0e8
Kritikasahota <sahotakritika029@gmail.com>
2026-07-08 12:09:35 +0530

    traffic light system using if-else statement

---
+++ Practice Questions/question5.js
@@ -0,0 +1,10 @@
+//Create a traffic light system that shows what to do based on a color
+let color = "red";
+
+if (color === "red") {
+  console.log("stop");
+} else if (color === "Yellow") {
+  console.log("slow");
+} else {
+  console.log("go");
+}
