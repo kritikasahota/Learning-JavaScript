7846dd6fdb6651d4874f9d315f0f5f271e650083
Kritikasahota <sahotakritika029@gmail.com>
2026-07-08 11:56:24 +0530

    demonstrated if-else statement for decision making

---
+++ 02_Conditional Statements/02_ ifElseStatement.js
@@ -0,0 +1,37 @@
+// ==============================
+// JavaScript if...else Statement
+// ==============================
+
+// The if...else statement is used to choose between two blocks of code.
+
+// Syntax:
+// if (condition) {
+//     // Executes if condition is true
+// } else {
+//     // Executes if condition is false
+// }
+
+// ------------------------------
+// Example 1: Voting Eligibility
+// ------------------------------
+
+let age = 25;
+
+if (age >= 18) {
+    console.log("You can vote");
+} else {
+    console.log("You cannot vote");
+}
+
+
+// ------------------------------
+// Example 2: Even or Odd Number
+// ------------------------------
+
+let num = 5;
+
+if (num % 2 === 0) {
+    console.log("Number is even");
+} else {
+    console.log("Number is odd");
+}
