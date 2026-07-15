920fc8493a61897b6c51835eb547e77ac983aabe
Kritikasahota <sahotakritika029@gmail.com>
2026-07-15 14:50:09 +0530

    good/bad number checker

---
+++ Practice Questions/question9.js
@@ -0,0 +1,8 @@
+//Create a number variable num with some value. Now, print “good” if the number is divisible by 10 and print “bad” 
+// if it is not.
+let num = 11;
+ if(num % 10 == 0){
+    console.log("Good");
+ }else{
+    console.log("Bad");
+ }
