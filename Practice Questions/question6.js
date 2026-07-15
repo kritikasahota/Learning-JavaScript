da579d306903d1bf5affca478de4740eb0c6d9bf
Kritikasahota <sahotakritika029@gmail.com>
2026-07-15 14:47:34 +0530

    popcorn price calculator using if-else

---
+++ Practice Questions/question6.js
@@ -0,0 +1,15 @@
+//Print price of popcorn according to its size
+
+let size = "XXL";
+
+if( size === "XL"){
+    console.log("Price is Rs.250")
+}else if( size === "L"){
+    console.log("Price is Rs.200")
+}else if( size === "M"){
+    console.log("Price is Rs.100")
+}else if( size === "S"){
+    console.log("Price is Rs.50")
+}else{
+    console.log("This size doesn't exist.")
+}
