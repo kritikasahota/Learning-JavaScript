89db345bf9add03e41ac68356efbb85feecf1e0e
Kritikasahota <sahotakritika029@gmail.com>
2026-07-15 14:51:04 +0530

    find the largest of 3 numbers

---
+++ Practice Questions/question11.js
@@ -0,0 +1,12 @@
+//Write a program to find the largest of 3 numbers
+let a = 2;
+let b = 5;
+let c = 4;
+
+if(a>b && a>c){
+   console.log("A is greater");
+} else if(b>a && b>c){
+    console.log("B is greater");
+}else{
+    console.log("C is greater");
+}
