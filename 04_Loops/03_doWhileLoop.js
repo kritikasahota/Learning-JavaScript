c8210e4ba4275064116ca150dbe9ec19436e2ae7
Kritikasahota <sahotakritika029@gmail.com>
2026-07-15 15:41:44 +0530

    Added Do While loop syntax and examples

---
+++ 04_Loops/03_doWhileLoop.js
@@ -0,0 +1,33 @@
+// ==========================================
+// DO...WHILE LOOP
+// ==========================================
+// A do...while loop executes the code at least once before checking the condition.
+
+// Syntax:
+// do {
+//     // code
+// } while (condition);
+
+// Example: Print numbers from 0 to 2
+
+let i = 0;
+
+do {
+    console.log(i);
+    i++;
+} while (i < 3);
+
+
+// ==========================================
+// ANOTHER EXAMPLE
+// ==========================================
+// Print numbers from 1 to 5
+
+let num = 1;
+
+do {
+    console.log(num);
+    num++;
+} while (num <= 5);
+
+
