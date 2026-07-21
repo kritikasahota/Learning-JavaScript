5dd5c6eec60336d97873c4771a3523bb2282cd96
Kritikasahota <sahotakritika029@gmail.com>
2026-07-21 10:27:28 +0530

    added functions, arguments, rest parameters, and passing objects/arrays

--- 07_Functions/01_functions.js
+++ 07_Functions/01_functions.js
@@ -1,59 +1,67 @@
 function sayMyName() {
-    console.log("k");
-    console.log("R");
-    console.log("I");
-    console.log("T");
-    console.log("I");
-    console.log("k");
-    console.log("A");
+  console.log("k");
+  console.log("R");
+  console.log("I");
+  console.log("T");
+  console.log("I");
+  console.log("k");
+  console.log("A");
 }
 sayMyName();
 
-function sum(a, b){
-    let sum = a + b;
-    console.log("Sum : ", sum);
-    
+function sum(a, b) {
+  let sum = a + b;
+  console.log("Sum : ", sum);
 }
 sum(3, 4);
 
 function loginUserMessage(username) {
-    if(username === undefined){
-        console.log("please enter a username")
-    }
-    console.log(`${username} has logged in`);
+  if (username === undefined) {
+    console.log("please enter a username");
+  }
+  console.log(`${username} has logged in`);
 }
 loginUserMessage("Kritika");
 
-//Rest operator
-function calculatePrice(...num){
-    return num
-}
-console.log(calculatePrice(200,400,500));
+// ==========================================
+// DEFAULT PARAMETERS
+// ==========================================
 
-function calculateCartPrice(val1, val2, ...num){
-    return num
-}
-console.log(calculateCartPrice(200, 400, 500, 2000));
+// Default parameters provide default values if no argument (or undefined) is passed.
 
-//Passing objects as parameters
+// Syntax:
+// function functionName(parameter = defaultValue) { }
+
+// Example
+function greet(name = "Guest") {
+  console.log(`Hello, ${name}`);
+}
+
+greet("Hitesh"); // Hello, Hitesh
+greet(); // Hello, Guest
+
+// ==========================================
+// Passing objects as parameters
+// ==========================================
 const user = {
-    name: "kritika",
-    price: 220
-}
-function handleObject(anyobject){
-     console.log(`username is ${anyobject.name} and price is ${anyobject.price}`)
+  name: "kritika",
+  price: 220,
+};
+function handleObject(anyobject) {
+  console.log(`username is ${anyobject.name} and price is ${anyobject.price}`);
 }
 handleObject(user);
 handleObject({
-    name: "tajinder",
-    price: 200
+  name: "tajinder",
+  price: 200,
 });
 
-//Passing arrays as parameters
+// ==========================================
+// Passing arrays as parameters
+// ==========================================
 const array = [1, 2, 3, 4];
-function returnSecondValue(getArray){
-     return getArray[1];
+function returnSecondValue(getArray) {
+  return getArray[1];
 }
 console.log(returnSecondValue(array));
 console.log(returnSecondValue([200, 400, 500, 1000]));
-
